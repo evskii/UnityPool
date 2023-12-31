dd3bdfe21a2c7d1673de32fb4960a48841a0d3f1
Evan Williams <evskimo97@gmail.com>
2023-12-31 14:04:41 +0000

    No idea whats in this push tbh

--- Pool/Assets/_Scripts/PoolManager.cs
+++ Pool/Assets/_Scripts/PoolManager.cs
@@ -31,18 +31,11 @@ public class PoolManager : MonoBehaviour
 	[SerializeField] private Image shotPowerImage;
 	private bool shotLinedUp = false;
 
-	[Header("Collision Prediction")]
-	[SerializeField] private int predictionRayCount = 10;
-	[SerializeField] private float collisionBoundsRadius = 0.18f;
-	private Vector3 predictedHitPoint;
-	private GameObject predictedHitBall;
-	
+	[Header("Collision Detection")]
+	[SerializeField] private float ballRadius = 0.09f;
 
 
-	// [SerializeField] private LayerMask ballLayer;
-	// [SerializeField] private LayerMask tableLayer;
-	
-	
+
 	private void Start() {
 		guideline = GetComponent<LineRenderer>();
 	}
@@ -90,15 +83,8 @@ public class PoolManager : MonoBehaviour
 				// Debug.Log("Shot Taken (Force: " + shotForce + ")");
 			}
 			
-			Debug.Log("Predicted hit pos: " + predictedHitPoint);
 			
 		}
-
-		if (!Input.GetMouseButton(0)) {
-			predictedHitPoint = GetPredictedHitPoint();
-			predictedHitBall = GetPredictedBallHit();
-		}
-		
 		//Other Shit
 		DrawGuideline();
 	}
@@ -108,105 +94,54 @@ public class PoolManager : MonoBehaviour
 		Vector3 ballWhiteGuidePos = ballWhite.transform.position;
 		guideline.SetPosition(0, ballWhiteGuidePos);
 
-		// if (shotLinedUp) {
-		// 	Vector3 mousePosMod = new Vector3(mouseStartPos.x, ballWhiteGuidePos.y, mouseStartPos.z);
-		// 	Vector3 dir = shotDir;
-		// 	dir.y = 0;
-		// 	guideline.SetPosition(1, ballWhiteGuidePos + dir.normalized * Vector3.Distance(ballWhite.transform.position, mousePosMod));
-		// } else {
-		// 	Vector3 mouseGuidePos = new Vector3(MousePos().x, ballWhite.transform.position.y, MousePos().z);
-		// 	guideline.SetPosition(1, mouseGuidePos);
-		// }
+		if (shotLinedUp) {
+			Vector3 mousePosMod = new Vector3(mouseStartPos.x, ballWhiteGuidePos.y, mouseStartPos.z);
+			Vector3 dir = shotDir;
+			dir.y = 0;
+			guideline.SetPosition(1, ballWhiteGuidePos + dir.normalized * Vector3.Distance(ballWhite.transform.position, mousePosMod));
+		} else {
+			Vector3 mouseGuidePos = new Vector3(MousePos().x, ballWhite.transform.position.y, MousePos().z);
+			guideline.SetPosition(1, mouseGuidePos);
+		}
 
-		if (predictedHitBall) {
-			// Debug.Log(GetPredictedBallHit().name);
-			Vector3 bounceOffDir = (predictedHitPoint - predictedHitBall.transform.position).normalized;
-			Vector3 bounceOffPos = predictedHitPoint - bounceOffDir * 1.5f;
+		if (GetPredictedCollisionPoint().hitObject != ballWhite) {
 			guideline.positionCount = 3;
-			guideline.SetPosition(2, bounceOffPos);
-			guideline.SetPosition(1, predictedHitPoint);
+			Vector3 hitPoint = GetPredictedCollisionPoint().hitPoint;
+			guideline.SetPosition(1, hitPoint);
+			guideline.SetPosition(2, hitPoint + (GetPredictedCollisionPoint().hitObject.transform.position - hitPoint).normalized * 2);
 		} else {
 			guideline.positionCount = 2;
-			if (shotLinedUp) {
-				Vector3 mousePosMod = new Vector3(mouseStartPos.x, ballWhiteGuidePos.y, mouseStartPos.z);
-				Vector3 dir = shotDir;
-				dir.y = 0;
-				guideline.SetPosition(1, ballWhiteGuidePos + dir.normalized * Vector3.Distance(ballWhite.transform.position, mousePosMod));
-			} else {
-				Vector3 mouseGuidePos = new Vector3(MousePos().x, ballWhite.transform.position.y, MousePos().z);
-				guideline.SetPosition(1, mouseGuidePos);
-			}
+		}
+	}
+
+	private struct PredictionData
+	{
+		public Vector3 hitPoint;
+		public GameObject hitObject;
+
+		public PredictionData(Vector3 point, GameObject obj) {
+			hitObject = obj;
+			hitPoint = point;
+		}
+	}
+	private PredictionData GetPredictedCollisionPoint() {
+		RaycastHit hit;
+		Vector3 mouse = Vector3.zero;
+		if (Input.GetMouseButton(0)) {
+			mouse = mouseStartPos;
+		} else {
+			mouse = MousePos();
 		}
 		
-		
-
-		// RaycastHit hit;
-		// Vector3 fixedMousPos = new Vector3(MousePos().x, ballWhite.transform.position.y, MousePos().z);
-		// Vector3 rayDir = shotLinedUp ? new Vector3(shotDir.x, 0, shotDir.z) : (fixedMousPos - ballWhite.transform.position).normalized;
-		// Debug.DrawRay(ballWhite.transform.position, (fixedMousPos - ballWhite.transform.position).normalized * 2, Color.red);
-		// if (Physics.Raycast(ballWhite.transform.position, rayDir, out hit, Mathf.Infinity, ballLayer)) {
-		// 	Debug.Log("HIT BALL: " + hit.transform.name);
-		// 	Vector3 contactDir = (hit.transform.position - hit.point).normalized;
-		// 	guideline.positionCount = 3;
-		// 	guideline.SetPosition(2, hit.point + contactDir * 5);
-		// }
-	}
-
-	private Vector3 GetPredictedHitPoint() {
-		Vector3 closestHitPoint = Vector3.zero;
-		for (int i = 0; i < predictionRayCount; i++) {
-			RaycastHit hitTest;
-			float x = collisionBoundsRadius * Mathf.Cos((360 / predictionRayCount) * i) + ballWhite.transform.position.x;
-			float z = collisionBoundsRadius * Mathf.Sin((360 / predictionRayCount) * i) + ballWhite.transform.position.z;
-			Vector3 rayStartPos = new Vector3(x, ballWhite.transform.position.y, z);
-			
-			Vector3 mouseModPos = MousePos();
-			mouseModPos.y = ballWhite.transform.position.y;
-
-			Vector3 rayDir = mouseModPos - ballWhite.transform.position;
-			rayDir = rayDir.normalized;
-			
-			Debug.DrawRay(rayStartPos, rayDir * 6);
-
-			if (Physics.Raycast(rayStartPos, rayDir, out hitTest, 100f, 1 << 7)) {
-				if (closestHitPoint == Vector3.zero) {
-					closestHitPoint = hitTest.point;
-				} else {
-					if (Vector3.Distance(ballWhite.transform.position, hitTest.point) < Vector3.Distance(ballWhite.transform.position, closestHitPoint)) {
-						closestHitPoint = hitTest.point;
-					}
-				}
-			}
+		mouse.y = ballWhite.transform.position.y;
+		Vector3 castDir = (mouse - ballWhite.transform.position).normalized;
+		if (Physics.SphereCast(ballWhite.transform.position, ballRadius, castDir, out hit, 100, 1 << 7)) {
+			return new PredictionData(hit.point, hit.transform.gameObject);
 		}
-		return closestHitPoint;
+		return new PredictionData(Vector3.zero, ballWhite);
 	}
 
-	private GameObject GetPredictedBallHit() {
-		GameObject closestHitBall = null;
-		for (int i = 0; i < predictionRayCount; i++) {
-			RaycastHit hitTest;
-			float x = collisionBoundsRadius * Mathf.Cos((360 / predictionRayCount) * i) + ballWhite.transform.position.x;
-			float z = collisionBoundsRadius * Mathf.Sin((360 / predictionRayCount) * i) + ballWhite.transform.position.z;
-			Vector3 rayStartPos = new Vector3(x, ballWhite.transform.position.y, z);
-			
-			Vector3 mouseModPos = MousePos();
-			mouseModPos.y = ballWhite.transform.position.y;
-
-			Vector3 rayDir = mouseModPos - ballWhite.transform.position;
-			rayDir = rayDir.normalized;
-
-			if (Physics.Raycast(rayStartPos, rayDir, out hitTest, 100f, 1 << 7)) {
-				if (closestHitBall == null) {
-					closestHitBall = hitTest.transform.gameObject;
-				} else {
-					if (Vector3.Distance(ballWhite.transform.position, hitTest.transform.position) < Vector3.Distance(ballWhite.transform.position, closestHitBall.transform.position)) {
-						closestHitBall = hitTest.transform.gameObject;
-					}
-				}
-			}
-		}
-		return closestHitBall;
-	}
+	
 	
 	private Vector3 MousePos() {
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
